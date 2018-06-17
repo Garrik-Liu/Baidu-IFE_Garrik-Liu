792f0e900496022f89c32e9981635698e8d0ec2d
Garrik-Liu <garrikliu@Gmail.com>
2018-06-17 11:36:26 -0400

    update

---
+++ 零基础学院/day17/main.js
@@ -0,0 +1,10 @@
+var contactMeBtn = document.getElementById("contactMe");
+var showMyHabitBtn = document.getElementById("showMyHabit");
+
+contactMeBtn.onclick = function() {
+    console.log("weixin: weixinmokan");
+}
+
+showMyHabitBtn.onclick = function() {
+    console.log("我爱喝可乐！");
+}
